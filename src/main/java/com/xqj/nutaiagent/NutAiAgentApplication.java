c1ebccae2ede8470cd120fa427ced6238eada69d
xuqingjian <xuqingjian894@gmail.com>
2025-05-12 10:19:30 +0800

    init commit

---
+++ src/main/java/com/xqj/nutaiagent/NutAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xqj.nutaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xqj/nutaiagent/NutAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqj.nutaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
