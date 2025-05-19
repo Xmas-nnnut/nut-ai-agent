7044a5a9d20047e686100d090434abea77846dcf
xuqingjian <xuqingjian894@gmail.com>
2025-05-19 15:27:56 +0800

    feat: 添加结构化输出报告功能

--- src/test/java/com/xqj/nutaiagent/app/LoveAppTest.java
+++ src/test/java/com/xqj/nutaiagent/app/LoveAppTest.java
@@ -31,4 +31,14 @@ class LoveAppTest {
         answer = loveApp.doChat(message, chatId);
         Assertions.assertNotNull(answer);
     }
+
+    @Test
+    void doChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是nut，我想让另一半（coconut）更爱我，但我不知道该怎么做";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
+    }
+
 }
